d6102f805a65cfcaa6fdb3d5e5b729463c883236
Mateo Montanez Baron <mateomontabaron@gmail.com>
2020-09-04 11:02:22 -0500

    Eslint

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './components/HelloWorld'
+
+// 2 params, component and the place where im going to push the component
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
